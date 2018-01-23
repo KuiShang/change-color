a136243f0a06c4aa9dd36056c795deb68cfefeb6
kk <shangkuikui@gomeplus.com>
2018-01-23 17:58:11 +0800

    first commit

---
+++ color.js
@@ -0,0 +1,22 @@
+const colorMap = {
+    '#B7A3FF': 'familyPrimary',
+    '#879FFF': 'familySecondary',
+    '#8981D8': 'sideBarTop'
+}
+
+const colors = [{
+    themeId: 1,
+    familyPrimary: '#B7A3FF',
+    familySecondary: '#879FFF',
+    sideBarTop: '#8981D8'
+}, {
+    themeId: 2,
+    familyPrimary: '#FDC5C5',
+    familySecondary: '#F070A0',
+    sideBarTop: '#E7829F'
+}, {
+    themeId: 3,
+    familyPrimary: '#414D6C',
+    familySecondary: '#2D1E3C',
+    sideBarTop: '#423C50'
+}]
